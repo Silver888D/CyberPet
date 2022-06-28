a831300370ea0b9038be9c6b6290566cde5f400d
Seb Amos <sebastian.amos8@gmail.com>
2022-06-28 17:42:17 +0100

    animals

--- animal.js
+++ animal.js
@@ -1,6 +1,9 @@
 class Animal {constructor(name) 
-    {this.name = name; this.health = 100; this.hunger = 100;}
-    drinks() {this.health += 5;return this;}
-    eats() {this.health += 5;this.hunger += 10;console.log(`${this.name}'s health is ${this.health}`);return this;}
+    {this.name = name; this.health = 100; this.hunger = 100; this.morale = 100}
+
+    play() {this.morale +=1;this.hunger -= 10; return this;}
+
+    eats() {this.health += 5;this.hunger += 10; this.morale +=2;console.log(`${this.name}'s health is ${this.health}`);return this;}
+
     stats() {return console.table({name: this.name, health: this.health, });}
-            }
+}
