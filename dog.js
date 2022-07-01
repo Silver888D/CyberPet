c006564c6f844e3b768943909f90d3c13a5b58d7
Seb Amos <sebastian.amos8@gmail.com>
2022-07-01 15:25:49 +0100

    downloadedprettier

--- dog.js
+++ dog.js
@@ -1,6 +1,7 @@
-const { Animal } = require('./animal.js');
+const { Animal } = require("./animal.js");
 
-async function petDied() {console.log(`
+async function petDied() {
+  console.log(`
     
     ⠀⠀⠀⢀⣠⠤⠔⠒⠒⠒⠒⠒⠢⠤⢤⣀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⢀⠴⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠲⣄⠀⠀⠀
@@ -18,31 +19,52 @@ async function petDied() {console.log(`
 ⠀⠀⠀⠀⠀⠀⠀⠀⢱⠀⠀⠀⢸⠘⡆⠀⠀⢸⣀⡰⠋⣆⠀⣠⠇
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⠤⠤⠼⠀⠘⠤⠴⠃⠀⠀⠀⠈⠉⠁⠀
     
-Your Pet has died as it's health has reached 0. GAME OVER! Run the file to try again.`);}
-    
-
-
+Your Pet has died as it's health has reached 0. GAME OVER! Run the file to try again.`);
+}
 
 class Dog extends Animal {
-    constructor(name) { super(name); }
-    fetch() {
-        this.morale += 20; this.hunger -= 20; this.health += 10; console.log(`${this.name} had fun exercising and is ${this.morale}`);
-        console.log(`   
+  constructor(name) {
+    super(name);
+  }
+  fetch() {
+    this.morale += 20;
+    this.hunger -= 20;
+    this.health += 10;
+    console.log(`${this.name} had fun exercising and is ${this.morale}`);
+    console.log(`   
         
      /\\__             
     (    @\\___        
    /          O      
   /    (_____/         
- /_____/    U`)
-            ; return this
-    };
-    petDeath() { if (this.health <= 0) { petDied() } }
-    
+ /_____/    U`);
+    return this;
+  }
+  petDeath() {
+    if (this.health <= 0) {
+      petDied();
+    }
+  }
 
-    healthDrain() { setInterval(() => { this.health -= 10 }, 1000); return this; }
-    // Error Handling - wrong option selected points are reduced 
-    mouseOnString() { this.morale -= 20; this.hunger -= 20; this.health -= 10; console.log(`${this.name} looks at you confused ${this.morale}`); }
-    obstacleCourse() { this.morale -= 20; this.hunger -= 20; this.health -= 10; console.log(`${this.name} tripped on the course ${this.morale}`); };
+  healthDrain() {
+    setInterval(() => {
+      this.health -= 10;
+    }, 1000);
+    return this;
+  }
+  // Error Handling - wrong option selected points are reduced
+  mouseOnString() {
+    this.morale -= 20;
+    this.hunger -= 20;
+    this.health -= 10;
+    console.log(`${this.name} looks at you confused ${this.morale}`);
+  }
+  obstacleCourse() {
+    this.morale -= 20;
+    this.hunger -= 20;
+    this.health -= 10;
+    console.log(`${this.name} tripped on the course ${this.morale}`);
+  }
 }
 
 module.exports = { Dog };
--- game.js
+++ game.js
@@ -1,39 +1,50 @@
-// Importing relevant JS files and inquirer 
-const inquirer = require('inquirer');
-const { Dog } = require('./dog.js');
-const { Cat } = require('./cat.js');
-const { Rabbit } = require('./rabbit.js');
+// Importing relevant JS files and inquirer
+const inquirer = require("inquirer");
+const { Dog } = require("./dog.js");
+const { Cat } = require("./cat.js");
+const { Rabbit } = require("./rabbit.js");
 
-
-// Variable 
+// Variable
 let myPet;
 
-// Start Function 
+// Start Function
 async function start() {
-    const { typeOfPet } = await inquirer.prompt
-        ({
-            type: 'list', name: 'typeOfPet', message: 'Please select your Pet ¬',
-            choices: [{ key: 'd', name: 'Dog', value: 'dog' }, { key: 'c', name: 'Cat', value: 'cat' }, { key: 'r', name: 'Rabbit', value: 'rabbit' },],
-        });
+  const { typeOfPet } = await inquirer.prompt({
+    type: "list",
+    name: "typeOfPet",
+    message: "Please select your Pet ¬",
+    choices: [
+      { key: "d", name: "Dog", value: "dog" },
+      { key: "c", name: "Cat", value: "cat" },
+      { key: "r", name: "Rabbit", value: "rabbit" },
+    ],
+  });
 
-    const { petName } = await inquirer.prompt({ type: 'input', name: 'petName', message: 'Please name your CyberPet >>' });
+  const { petName } = await inquirer.prompt({
+    type: "input",
+    name: "petName",
+    message: "Please name your CyberPet >>",
+  });
 
-    if (typeOfPet === 'dog') myPet = new Dog(petName);
-    else if (typeOfPet === 'cat') myPet = new Cat(petName);
-    else if (typeOfPet === 'rabbit') myPet = new Rabbit(petName);
-    if (typeOfPet === 'dog') console.log(`
+  if (typeOfPet === "dog") myPet = new Dog(petName);
+  else if (typeOfPet === "cat") myPet = new Cat(petName);
+  else if (typeOfPet === "rabbit") myPet = new Rabbit(petName);
+  if (typeOfPet === "dog")
+    console.log(`
      ^..^      /
      /_/\\_____/
         /\\   /\\
        /  \\ /  \\
     `);
-    if (typeOfPet === 'cat') console.log(`   
+  if (typeOfPet === "cat")
+    console.log(`   
      \\   /\\
      )  ( ')
      (  /  )
       \\(__)|`);
 
-    if (typeOfPet === 'rabbit') console.log(`
+  if (typeOfPet === "rabbit")
+    console.log(`
       ^
      / \\
     / _ \\
@@ -58,25 +69,28 @@ async function start() {
        |     | 
     `);
 
-    userChoice();
+  userChoice();
 }
 
 async function userChoice() {
-    const { choices } = await inquirer.prompt
-        ({
-            type: 'list', name: 'choices', message: 'Please choose an care option for your CyberPet >>',
-            choices: [
-                { key: 'f', name: 'Feed', value: 'feed' },
-                { key: 'ft', name: 'Fetch', value: 'fetch' },
-                { key: 'ms', name: 'Mouse', value: 'mouseOnString' },
-                { key: 'oc', name: 'Obstacle', value: 'obstacleCourse' },
-                { key: 'v', name: 'Vet', value: 'vet' },
-                { key: 's', name: 'Stats', value: 'stats' },
-                { key: 'q', name: 'Quit', value: 'quit' }
-            ],
-        });
+  const { choices } = await inquirer.prompt({
+    type: "list",
+    name: "choices",
+    message: "Please choose an care option for your CyberPet >>",
+    choices: [
+      { key: "f", name: "Feed", value: "feed" },
+      { key: "ft", name: "Fetch", value: "fetch" },
+      { key: "ms", name: "Mouse", value: "mouseOnString" },
+      { key: "oc", name: "Obstacle", value: "obstacleCourse" },
+      { key: "v", name: "Vet", value: "vet" },
+      { key: "s", name: "Stats", value: "stats" },
+      { key: "q", name: "Quit", value: "quit" },
+    ],
+  });
 
-    if (choices === 'feed'&& myPet.health > 0) await myPet.feed(), console.log(`
+  if (choices === "feed" && myPet.health > 0)
+    await myPet.feed(),
+      console.log(`
            
             ███████╗░█████╗░████████╗
             ██╔════╝██╔══██╗╚══██╔══╝
@@ -84,27 +98,38 @@ async function userChoice() {
             ██╔══╝░░██╔══██║░░░██║░░░
             ███████╗██║░░██║░░░██║░░░
             ╚══════╝╚═╝░░╚═╝░░░╚═╝░░░            
-             `),userChoice();
-    if (choices === 'fetch' && myPet.health > 0) await myPet.fetch(),userChoice();
-    if (choices === 'mouseOnString' && myPet.health > 0) await myPet.mouseOnString(),userChoice();
-    if (choices === 'obstacleCourse' && myPet.health > 0) await myPet.obstacleCourse(),userChoice();
-    if (choices === 'stats' && myPet.health > 0) await myPet.stats(),userChoice();
-    if (choices === 'quit') { const quitChoice = await confirmQuit(); if (quitChoice) return; }
-    
-    myPet.healthDrain();
-    
-    myPet.petDeath();
+             `),
+      userChoice();
+  if (choices === "fetch" && myPet.health > 0)
+    await myPet.fetch(), userChoice();
+  if (choices === "mouseOnString" && myPet.health > 0)
+    await myPet.mouseOnString(), userChoice();
+  if (choices === "obstacleCourse" && myPet.health > 0)
+    await myPet.obstacleCourse(), userChoice();
+  if (choices === "stats" && myPet.health > 0)
+    await myPet.stats(), userChoice();
+  if (choices === "quit") {
+    const quitChoice = await confirmQuit();
+    if (quitChoice) return;
+  }
+
+  myPet.healthDrain();
+
+  myPet.petDeath();
 }
 
 async function confirmQuit() {
-    const { quitChoice } = await inquirer.prompt
-        ({
-            type: 'list', name: 'quitChoice', message: 'Are you sure you want to quit? >>',
-            choices: [{ key: 'y', name: 'Yes', value: 'yes' }, { key: 'n', name: 'No', value: 'no' }]
-        });
-    if (quitChoice === 'yes') await start();
-    else if (quitChoice === 'no') await userChoice();
+  const { quitChoice } = await inquirer.prompt({
+    type: "list",
+    name: "quitChoice",
+    message: "Are you sure you want to quit? >>",
+    choices: [
+      { key: "y", name: "Yes", value: "yes" },
+      { key: "n", name: "No", value: "no" },
+    ],
+  });
+  if (quitChoice === "yes") await start();
+  else if (quitChoice === "no") await userChoice();
 }
 
-
-start(); 
+start();
